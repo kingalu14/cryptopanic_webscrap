f0dabb6fe04c39fd8d4e097c83240e82144e4d6f
kingalu14 <kamiraki@gmail.com>
2020-04-30 11:10:10 +0300

    add template engine

--- app.js
+++ app.js
@@ -1,44 +1,33 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+var bodyParser = require('body-parser')
 const getPostDetail = require('./get_post_details');
-const port =  8080;
+const port =  process.env.PORT || 3030;
 
 const apiUrl = 'https://cryptopanic.com/api/v1/posts/?auth_token=f856962ef8e8651d078317072d4667b829f4e3dd&public=true';
 //const apiUrl = 'https://cryptopanic.com/';
 
-app.get('/', (req , res) => {
-    // Make a request for a user with a given ID
-    axios.get(apiUrl)
-    .then(async (response) => {
-            var newsSize = 20;
-           for(i=0;i<newsSize;i++){
-                res.write('Title:');
-                res.write(response.data['results'][i]['title']);
-                res.write('\n');
-                var details = await getPostDetail(response.data['results'][i]['url']);
-                console.log(details);
-                res.write('Detail:');
-                res.write(details);
-                res.write('\n');
+app.set('view engine', 'ejs');
 
-                res.write('Created at:');
-                res.write(response.data['results'][i]['created_at']);
-                res.write('\n')
-                res.write('\n');
-                res.write('\n');
-                
-           }
-            
-         
-            res.end();
-     
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req , res) => {
+    axios.get(apiUrl)
+    .then(response => {
+        res.render('index',{data: response.data.results});
     })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    });
-    
+    .catch(error => {
+        console.log('error');
+    })
+});
+
+app.post('/getpostdata',  async (req,res) => {
+  /*   const url = req.body.url; */
+  console.log(req.body);
+    const info = await getPostDetail(req.body.link);
+    res.send(info)
 });
 
 /*  app listen*/
